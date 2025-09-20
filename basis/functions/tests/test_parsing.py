fd7297b188d8a66a94e7f2898cc3e04bbf2c6e1f
DjamelEddine <eddine29@icloud.com>
2025-09-20 16:14:34 +0100

    Create test_parsing.py

---
+++ basis/functions/tests/test_parsing.py
@@ -0,0 +1,16 @@
+import pytest
+from parsing import parse_filename
+
+def test_parse_valid_filename():
+    text, commands = parse_filename('return(type["hello"].UPPER.lower)')
+    assert text == "hello"
+    assert commands == ["UPPER", "lower"]
+
+def test_parse_no_commands():
+    text, commands = parse_filename('return(type["hi"])')
+    assert text == "hi"
+    assert commands == []
+
+def test_parse_invalid_filename():
+    with pytest.raises(ValueError):
+        parse_filename("wrongformat.th")
