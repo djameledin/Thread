a626c52908475723f184c0de001b6e82cd97cc41
DjamelEddine <eddine29@icloud.com>
2025-09-20 18:45:20 +0100

    Update test_validation.py

--- basis/functions/tests/test_validation.py
+++ basis/functions/tests/test_validation.py
@@ -1,11 +1,12 @@
-import os, tempfile
-from validation import find_th_files
+import pytest
+from ..validation import find_th_files
 
 def test_find_th_files(tmp_path):
     th_file = tmp_path / "test.th"
     th_file.write_text("dummy")
 
     found_files = find_th_files(tmp_path)
+
     assert str(th_file) in found_files
 
 def test_find_no_files(tmp_path):
